eacfcaa2458b2fb188c111c7e951a6c72ea0bfc9
Ashwin S K <ashevents24@gmail.com>
2022-07-30 21:46:27 +0530

    firebase for web

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:seta/responsive/mobile_screen_layout.dart';
 import 'package:seta/responsive/responsive_layout_screen.dart';
 import 'package:seta/responsive/web_screen_layout.dart';
 import 'package:seta/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyBhQ9a9qlC6qh3wpbsdO9UfN9tiRlnKNnM',
+        appId: '1:391759904308:web:ddda3ed2585a084a125461',
+        messagingSenderId: '391759904308',
+        projectId: 'seta2022-9aaed',
+        storageBucket: "seta2022-9aaed.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
