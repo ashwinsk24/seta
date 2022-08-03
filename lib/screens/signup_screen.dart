14456a241fb1ba77234675e198549d113918cfb1
Ashwin S K <ashevents24@gmail.com>
2022-08-03 17:31:34 +0530

    page overpixel fixed

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -85,6 +85,7 @@ class _SignupScreenState extends State<SignupScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Container(
           padding: const EdgeInsets.symmetric(horizontal: 32),
