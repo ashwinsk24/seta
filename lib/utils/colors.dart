b66e910375386eaf6cb94af8fef7114395cefeb3
Ashwin S K <ashevents24@gmail.com>
2022-08-18 22:10:36 +0530

    botton ui

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 const mobileBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
 const webBackgroundColor = Color.fromRGBO(18, 18, 18, 1);
 const mobileSearchColor = Color.fromRGBO(38, 38, 38, 1);
-const blueColor = Color.fromRGBO(0, 149, 246, 1);
+const blueColor = Color.fromRGBO(252, 186, 3, 1);
 const primaryColor = Colors.white;
 const secondaryColor = Colors.grey;
